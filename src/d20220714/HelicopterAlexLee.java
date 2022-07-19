1cb98cad874bce328893a4524fe763e177f6aa29
AntonVotinov <votinovanton@gmail.com>
2022-07-19 14:25:29 +0300

    Commit from Dell

--- src/d20220714/HelicopterAlexLee.java
+++ src/d20220714/HelicopterAlexLee.java
@@ -3,9 +3,16 @@ package d20220714;
 public class HelicopterAlexLee {
     public static void main(String[] args) throws InterruptedException {
         while (true) {
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
             System.out.println("---+++");
             System.out.println("+++---");
-            Thread.sleep(500);
+            Thread.sleep(100);
+            System.out.println("+++---");
+            System.out.println("---+++");
+            Thread.sleep(100);
         }
     }
 }
